b87089d47fd3231e14d8301a0136ca275bafbd10
Dominic Muttel <dominic.muttel@gmail.com>
2015-10-20 16:05:31 -0600

    completed

--- 00_functions/src.js
+++ 00_functions/src.js
@@ -1,10 +1,44 @@
 module.exports = {
 
+
   helloWorld: function(){
-    // fill me in
+    return "Hello World";
   },
 
   hello: function(string){
-    // fill me in
+    return "Hello " + string;
+  },
+
+  shout: function(string){
+    var str = string.toUpperCase();
+    return "Hello " + str;
+  },
+
+  whisper: function(string){
+    var str = string.toLowerCase();
+    return "Hello " + str;
+  },
+
+  separate: function(string){
+    return string.split('');
+  },
+
+  reverseJoin: function(array){
+    array.reverse();
+    var string = array.join(',');
+    return string;
+  },
+
+  reverseJoinCompact: function(array){
+    // array.reverse();
+    // var string = array.join('');
+    // return string;
+    return array.reverse().join('');
+  },
+
+  backwardsDay: function(string){
+    return "Hello " + string.split('').reverse().join('');
   }
-}
+
+
+};

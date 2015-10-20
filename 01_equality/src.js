1f90f1135dc5a920093e1e118bf0b47005da571f
Dominic Muttel <dominic.muttel@gmail.com>
2015-10-20 16:28:06 -0600

    completed

--- 01_equality/src.js
+++ 01_equality/src.js
@@ -1,5 +1,42 @@
 module.exports = {
   sameLength: function(string1, string2) {
-    // your code here:
-  }
-}
+    if (string1.length === string2.length){
+      return true;
+    } else{
+      return false;
+    }
+  },
+
+  firstGreater: function(string1, string2){
+    if (string1 > string2) {
+      return true;
+    } else{
+      return false;
+    }
+  },
+
+  firstGreaterOrEqualTo: function(string1, string2){
+    if (string1 > string2) {
+      return true;
+    } else{
+      return false;
+    }
+  },
+
+  firstLengthGreater: function(string1, string2){
+    if (string1.length > string2.length) {
+      return true;
+    } else{
+      return false;
+    }
+  },
+
+  secondGreater: function(string1, string2){
+    if (string1 < string2) {
+      return true;
+    } else{
+      return false;
+    }
+  },
+
+};

0ec79a25a9ea996be575b7990a917c6aa564c20d
Dominic Muttel <dominic.muttel@gmail.com>
2015-10-23 15:56:39 -0600

    complete

--- 04_objects/src.js
+++ 04_objects/src.js
@@ -1,5 +1,40 @@
 module.exports = {
   person: function() {
-    // your code here
+   var person = {firstName: 'John',
+    lastName: 'Mayer'};
+    return person;
+  },
+
+  sweetnessProperty: function(obj) {
+      return obj.sweetness;
+  },
+
+  savorynessProperty: function(obj) {
+      return obj['savory-ness'];
+  },
+
+  keys: function(obj){
+    var keyNames = [];
+    for (var i in obj) {
+        keyNames.push(i);
+    }
+    return keyNames;
+  },
+
+  sortedKeys: function(obj){
+    var keyNames = [];
+    for (var i in obj) {
+      keyNames.push(i);
+    }
+    return keyNames.sort();
+  },
+
+  reverseSortedKeys: function(obj){
+    var keyNames = [];
+    for (var i in obj) {
+      keyNames.push(i);
+    }
+    return keyNames.sort().reverse();
   }
-}
+
+};

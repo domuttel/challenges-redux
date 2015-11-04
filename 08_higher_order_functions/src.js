6c5d0b672e0db58139abfd2c9a05bd1d66d67263
Dominic Muttel <dominic.muttel@gmail.com>
2015-11-03 19:58:08 -0700

    complete

--- 08_higher_order_functions/src.js
+++ 08_higher_order_functions/src.js
@@ -1,6 +1,35 @@
 module.exports = {
 
   simpleCall: function(callback){
-    // your code here
-  }
-}
+    // console.log(callback());
+    return callback();
+  },
+
+  each: function(array, callback) {
+    // console.log(array.forEach(callback));
+    return array.forEach(callback);
+  },
+
+  map: function(array, callback){
+    // console.log(array.map(callback));
+    return array.map(callback);
+  },
+
+  reduce: function(array, num, callback){
+    // console.log(array.reduce(callback, num));
+    return array.reduce(callback, num);
+  },
+
+  find: function(array, callback){
+    return array.find(callback);
+  },
+
+  any: function(array, callback){
+    return array.some(callback);
+  },
+
+  all: function(array, callback){
+    return array.every(callback);
+  },
+
+};
